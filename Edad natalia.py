be1f066e5c341c3a5e9d9ef9fb3f6e0ca4935eda
natalia239 <nataliafabra08@gmail.com>
2026-04-14 15:43:19 -0500

    Add files via upload

---
+++ Edad natalia.py
@@ -0,0 +1,22 @@
+# Natalia Fabra Meza Log
+
+
+
+
+# Declaracion con TIPADO DINÁMICO
+nombre = "natalia fabra meza"
+edad = 21
+altura = 1.70
+peso = 54.30
+
+print(f"Tipado dinámico: {nombre} tiene {edad} años y mide"
+      
+      f"{altura} metros")
+
+# Declaracion explicita 
+nombre= str("natalia")
+edad= int(21)
+altura= float(1.70)
+peso= float(54.30)
+
+print (f"Declaracion explicita: {nombre} tiene {edad} años y mide {altura}metros")
